44a30aa39c099aae06d5e0ebc52f7637811ccaf1
kyawakyawa <kyawashell@gmail.com>
2021-01-09 01:31:44 +0900

    Zn

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod degree;
 mod scalar;
 
+use degree::Zn;
 use scalar::Integer;
 use std::time::Instant;
 
@@ -18,5 +19,16 @@ fn main() {
         println!("{}!={}", _i, factorial(&Integer::from(_i)));
     }
     let end = start.elapsed();
+
     println!("{}.{:03}sec", end.as_secs(), end.subsec_nanos() / 1_000_000);
+
+    let z0 = Zn::from(vec![Integer::from(0), Integer::from(1), Integer::from(2)]);
+    let z1 = Zn::from(vec![Integer::from(3), Integer::from(4), Integer::from(5)]);
+    let z2 = z0.clone();
+
+    println!("{}", &z0 + &z1);
+    println!("{:?}", &z0 - &z1);
+    println!("{:?}", z2);
+
+    println!("{:?}", z0.cmp(&z1));
 }
